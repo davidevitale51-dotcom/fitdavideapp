e4f9318d9570de4e9136798fb32e19fb2206f069
davidevitale51-dotcom <davide.vitale51@gmail.com>
2026-05-31 09:23:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* Service worker – offline cache pro Kalorický Tracker */
+const CACHE = 'kalorie-v1';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest',
+  './icon-180.png', './icon-192.png', './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // jen vlastní soubory cachujeme; cizí (např. Open Food Facts EAN) necháme projít
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
